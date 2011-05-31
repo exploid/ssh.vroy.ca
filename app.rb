627c3502a0bcbf43d0e1606d9b2a3602e6d0b201
Vincent Roy <vincentroy8@gmail.com>
2011-05-31 19:38:03 -0300

    Improve how the color stripping is handled.

--- app.rb
+++ app.rb
@@ -12,9 +12,8 @@ require "net/ssh/shell"
 
 class Net::SSH::Shell
   # Method that returns the [exit_status, string_result] of a command.
-  def exec!(cmd)
-    # For now, just make sure that there are no colors in the ls output
-    cmd.gsub!(/^ls ?/, "ls --color=none ")
+  # Strips out some known terminal color patterns when skip_color=true
+  def exec!(cmd, strip_color=true)
     result = ""
     res = self.execute( cmd ) do |c|
       c.on_output do |process, string|
@@ -22,6 +21,24 @@ class Net::SSH::Shell
       end
     end
     self.wait!
+
+    if strip_color
+      # The color pattern only matches the regex when inspected
+      result = result.inspect
+
+      # Go through some known color patterns and strop them out of the result.
+      [
+       /\\e\[\d{1,2};\d{1,2}m/im, # Matches \e[01;34m
+       /\\e\[\d{1}m/im, # Matches \e[0m
+      ].each do |regex|
+        result.gsub!(regex, "")
+      end
+
+      # Uninspect the result once the colors have been removed.
+      # This is so things like \r\n are returned properly as \r\n etc.
+      result = eval(result) # How safe is this?
+    end
+
     return [ res.exit_status, result ]
   end
 end
--- controller/main.rb
+++ controller/main.rb
@@ -14,15 +14,13 @@ class MainController < Ramaze::Controller
       creds[key] = value
     end
 
-    # Connect
+    # Connect and init shell
     session[:connection] = Net::SSH.start(nil, nil, creds)
     session[:shell] = session[:connection].shell
 
-    # Get pwd for first prompt display
-    session[:pwd] = session[:shell].exec!("pwd").last.gsub("\r\n", "")
+    execute_pwd # Set pwd for first prompt display
 
-    # Do not store password along with the credentials
-    creds.delete(:password)
+    creds.delete(:password) # Do not store password along with the credentials
     session[:credentials] = creds
 
     redirect Rs(:ssh)
@@ -41,22 +39,35 @@ class MainController < Ramaze::Controller
 
   deny_layout :command
   def command
-    command = h(request[:command])
-    
-    exit_status, result = session[:shell].exec!( command )
-    session[:pwd] = session[:shell].exec!("pwd").last
+    # request[:command] is not escaped so that this executes: `echo "Test"`
+    exit_status, result = execute_cmd( request[:command] )
+    execute_pwd # Set pwd again in case it has changed after running the command
 
-    result = h(result) # sanitize the output
-
-    result.gsub!("\r\n", "<br/>")
-    result.gsub!("\t", "        ")
-    result.gsub!(" ", "&nbsp;")
-    session[:pwd].gsub!("\r\n", "")
-    
     return { :result => result, :pwd => session[:pwd] }.to_json
   rescue Exception => e
     puts e
     return { :result => "Something wrong happened, please connect again." }.to_json
+  end
+
+  private
+
+  # Helper method that sets session[:pwd]. Runs session[:shell].exec!("pwd")
+  # and prepare the output for the web.
+  def execute_pwd
+    session[:pwd] = session[:shell].exec!("pwd").last.gsub("\r\n", "")
+  end
+
+  # Helper method to run a command on session[:shell] with #exec! and sanitize it for the web.
+  def execute_cmd(cmd)
+    exit_status, result = session[:shell].exec!( cmd )
+
+    # Prepare the output to be displayed on the web.
+    result = h(result)
+    result.gsub!("\r\n", "<br/>")
+    result.gsub!("\t", "        ")
+    result.gsub!(" ", "&nbsp;")
+
+    return exit_status, result
   end
 
 end
